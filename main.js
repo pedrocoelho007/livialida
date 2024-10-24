1f371d189e1358170db223de9c0f066387bdd7d8
as vezes me sinto o justin <142445197+pedrocoelho007@users.noreply.github.com>
2024-10-24 09:21:33 -0300

    f

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-function criaCartao (categoria, pergunta, resposta){
-let container = document.getElementById('container');
-let cartao = document.createElement('article');
-cartao.className = 'cartao';
-cartao.innerHTML =`conteudo-cartao` 
-cartao.innerHTML =´
-<div class="conteudo-cartao">
-    <h3>categoria</h3>
+function criacartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao';
+    cartao.innerHTML = `
+        <div class="conteudo-cartao" >
+       <h3>${categoria}</h3>
     <div class="pergunta-cartao">
-    <p>pergunta</p>
+        <p>${pergunta}</p>
     </div>
     <div class="resposta-cartao">
-        <p>resposta</p>
+        <p>${resposta}</p>
     </div>
-</div>
-container.appendChild(cartao);
+</div >
+        `
+    container.appendChild(cartao);
+}
