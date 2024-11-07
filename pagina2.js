8b233599b256e3d2a0f8ac2dcb243ec3672b7e92
as vezes me sinto o justin <142445197+pedrocoelho007@users.noreply.github.com>
2024-11-07 09:01:21 -0300

    er

--- pagina2.js
+++ pagina2.js
@@ -16,7 +16,7 @@ function criacartao(categoria, pergunta, resposta) {
     container.appendChild(cartao);
 }
     function mostraResposta(card) {
-        const resposta = card.quenySelector('.resposta-cartao');
+        const resposta = card.querySelector('.resposta-cartao');
         if(resposta.style.display === 'none'){
             resposta.style.display = 'block'
         }
